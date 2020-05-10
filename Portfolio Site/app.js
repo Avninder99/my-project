92fbaec1dbe0ee2ac9710ad6a49a8896416d3d48
Avninder Preet Singh <53931646+Avninder99@users.noreply.github.com>
2020-05-10 20:47:28 +0530

    add  new project section

--- Portfolio Site/app.js
+++ Portfolio Site/app.js
@@ -64,3 +64,101 @@ $(function () {
         }
     });
 });
+
+// project card section code
+
+// card selector
+const card1 = document.querySelector(".card1"),
+      card2 = document.querySelector(".card2"),
+      card3 = document.querySelector(".card3"),
+      card4 = document.querySelector(".card4"),
+      card5 = document.querySelector(".card5"),
+      card6 = document.querySelector(".card6");
+
+//heading1 selectors
+const heading1_1 = document.querySelector(".heading1-1"),
+      heading1_2 = document.querySelector(".heading1-2"),
+      heading1_3 = document.querySelector(".heading1-3"),
+      heading1_4 = document.querySelector(".heading1-4"),
+      heading1_5 = document.querySelector(".heading1-5"),
+      heading1_6 = document.querySelector(".heading1-6");
+
+//heading2 selectors
+const heading2_1 = document.querySelector(".heading2-1"),
+      heading2_2 = document.querySelector(".heading2-2"),
+      heading2_3 = document.querySelector(".heading2-3"),
+      heading2_4 = document.querySelector(".heading2-4"),
+      heading2_5 = document.querySelector(".heading2-5"),
+      heading2_6 = document.querySelector(".heading2-6");
+
+//para selectors
+const para1_1 = document.querySelector(".para1-1"),
+      para1_2 = document.querySelector(".para1-2"),
+      para1_3 = document.querySelector(".para1-3"),
+      para1_4 = document.querySelector(".para1-4"),
+      para1_5 = document.querySelector(".para1-5"),
+      para1_6 = document.querySelector(".para1-6");
+      
+
+card1.addEventListener("mouseenter",function(){
+    heading1_1.classList.add("changer1");
+    heading2_1.classList.add("changer2");
+    para1_1.classList.add("changer3");
+});
+card2.addEventListener("mouseenter",function(){
+    heading1_2.classList.add("changer1");
+    heading2_2.classList.add("changer2");
+    para1_2.classList.add("changer3");
+});
+card3.addEventListener("mouseenter",function(){
+    heading1_3.classList.add("changer1");
+    heading2_3.classList.add("changer2");
+    para1_3.classList.add("changer3");
+});
+card4.addEventListener("mouseenter",function(){
+    heading1_4.classList.add("changer1");
+    heading2_4.classList.add("changer2");
+    para1_4.classList.add("changer3");
+});
+card5.addEventListener("mouseenter",function(){
+    heading1_5.classList.add("changer1");
+    heading2_5.classList.add("changer2");
+    para1_5.classList.add("changer3");
+});
+card6.addEventListener("mouseenter",function(){
+    heading1_6.classList.add("changer1");
+    heading2_6.classList.add("changer2");
+    para1_6.classList.add("changer3");
+});
+
+
+card1.addEventListener("mouseleave",function(){
+    heading1_1.classList.remove("changer1");
+    heading2_1.classList.remove("changer2");
+    para1_1.classList.remove("changer3");
+});
+card2.addEventListener("mouseleave",function(){
+    heading1_2.classList.remove("changer1");
+    heading2_2.classList.remove("changer2");
+    para1_2.classList.remove("changer3");
+});
+card3.addEventListener("mouseleave",function(){
+    heading1_3.classList.remove("changer1");
+    heading2_3.classList.remove("changer2");
+    para1_3.classList.remove("changer3");
+});
+card4.addEventListener("mouseleave",function(){
+    heading1_4.classList.remove("changer1");
+    heading2_4.classList.remove("changer2");
+    para1_4.classList.remove("changer3");
+});
+card5.addEventListener("mouseleave",function(){
+    heading1_5.classList.remove("changer1");
+    heading2_5.classList.remove("changer2");
+    para1_5.classList.remove("changer3");
+});
+card6.addEventListener("mouseleave",function(){
+    heading1_6.classList.remove("changer1");
+    heading2_6.classList.remove("changer2");
+    para1_6.classList.remove("changer3");
+});
